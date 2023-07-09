16da4b089b8a6718ad3a80ac04d5e3a85b6e4a8a
Ikshan <ikshanbhardwaj.2003@gmail.com>
2023-07-09 13:17:45 +0530

    made the script

---
+++ main.py
@@ -0,0 +1,80 @@
+"""
+This data in is in txt format with tab seperated values.
+"""
+
+import pandas as pd
+import re
+import nltk
+# nltk.download('book')s
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+data = pd.read_csv('smsspamcollection/SMSSpamCollection', sep='\t', names=['label', 'message'])
+
+# Data Cleaning and Preprocessing
+lemmatizer = WordNetLemmatizer()
+corpus = []
+for i in range(len(data)):
+    new = re.sub('[^a-zA-Z]',' ',data["message"][i])
+    new = new.lower()
+    new = new.split()
+
+    new = [lemmatizer.lemmatize(word) for word in new if not word in set(stopwords.words('english'))]
+    new = ' '.join(new)
+    corpus.append(new)
+# print(corpus)
+
+# Creating the TF-IDF model
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+cv = TfidfVectorizer(max_features = 2500) 
+"""
+    we are restricting the number of features/words to top 2500 most frequent, because there will be many 
+    words that only occur once and do  not place that important of a role
+"""
+
+X = cv.fit_transform(data["message"]).toarray()
+
+print(len(X)) 
+""" 
+    Number of sentences there are in the dataset 
+"""
+print(len(X[1])) 
+"""
+    Number of words there are in the dataset 
+"""
+
+""" Now we will convert the label column ->'ham' 'spam' to dummy variables i.e 0 and 1's """
+
+y = pd.get_dummies(data["label"])
+# print(y) 
+
+"""
+we can see that it makes two columns ham and spam where in true and false are mentioned, 
+we'll take just one column rather than the whole two columns 
+"""
+y = y.iloc[:,1].values
+# print(y)
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Training model using Naive bayes classifier
+
+from sklearn.naive_bayes import MultinomialNB
+spam_detect_model = MultinomialNB().fit(X_train, y_train)
+
+y_pred=spam_detect_model.predict(X_test)
+
+"""
+Getting the accuracy of my model
+"""
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_pred,y_test)
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(y_test,y_pred)
+
+print("Accuracy of my model is :",accuracy)
